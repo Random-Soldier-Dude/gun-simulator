4cbc1352d5d3ce93c2d1f98c76be994e34ef65a3
Yew Rei An <randomsoldierdude@gmail.com>
2023-05-31 12:35:46 +0800

    Initial Commit

---
+++ gun simulator/gun_simulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  gun_simulatorApp.swift
+//  gun simulator
+//
+//  Created by Yew Rei An on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct gun_simulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
